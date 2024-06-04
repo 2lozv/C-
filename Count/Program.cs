30923fedb6b94616b1fe5d326ba7fc165c681ff8
2lozv <kou.ktmf@icloud.com>
2024-06-04 16:45:16 +0900

    20240604

---
+++ Count/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("文字数カウント");
+            Console.WriteLine("文字を入力してください");
+
+            string text = Console.ReadLine();
+
+            int count = text.Length;
+
+            Console.WriteLine ("文字数 : {0}",count);
+        }
+    }
+}
