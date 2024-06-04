813624fc52fe558726253221fdf7fc97433eedf3
2lozv <kou.ktmf@icloud.com>
2024-06-04 17:03:52 +0900

    second commit

---
+++ HighLow/Darts/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighLow
+{
+    public class Chara
+    {
+        //フィールドの定義
+        protected string name;
+        protected int number;
+
+        //コンストラクタ設定
+        public Chara(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Number
+        {
+            set { number = value; }
+            get { return number; }
+        }
+
+        public void show()
+        {
+            Console.WriteLine("こんにちは{0}さん", this.name);
+        }
+
+        public void show(string name, int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("{0}はHigh選択しました", name);
+                    break;
+
+                case 2:
+                    Console.WriteLine("{0}はLowを選択しました", name);
+                    break;
+            }
+
+        }
+
+        //public int num()
+        //{
+        //    Random rnd = new Random();
+        //    int num = rnd.Next(1, 14);
+        //    return num;
+        //}
+    }
+}
---
+++ HighLow/Darts/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighLow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("High&Lowゲーム");
+
+            Console.WriteLine("");
+
+            Chara me = new Chara("ふうか");
+            me.show();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Start!");
+
+            int score = 0;
+
+            while (true)
+            {
+                score++;
+
+                Console.WriteLine("");
+
+                //最初の数字を表示
+                Random rnd = new Random();
+                int num = rnd.Next(1, 14);
+                Console.WriteLine("↑↑↑↑↑ {0} ↓↓↓↓↓", num);
+
+                Console.WriteLine("");
+
+                Console.WriteLine("High? Low?");
+
+                Console.WriteLine("");
+
+                //入力内容を待つ
+                Console.WriteLine("1→High　2→Low");
+
+                Console.WriteLine("");
+
+                me.Number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                //入力内容を確認
+                me.show(me.Name, me.Number);
+
+                Console.WriteLine("");
+
+                Console.WriteLine("数字を表示");
+                Console.ReadKey();
+
+                Console.WriteLine("");
+
+                Random rnd2 = new Random();
+                int num2 = rnd2.Next(1, 14);
+                Console.WriteLine("【{0}】", num2);
+
+                Console.WriteLine("");
+
+                //条件分岐
+                if (num == num2)
+                {
+                    Console.WriteLine("DRAW");
+                    continue;
+                }
+                else if (num < num2)
+                {
+                    if (me.Number == 1)
+                    {
+                        Console.WriteLine("High 正解です！");
+                    }
+                    else
+                    {
+                        Console.WriteLine("High 選択ミス！ゲーム終了");
+                        break;
+                    }
+                }
+                else if (num > num2)
+                {
+                    if (me.Number == 2)
+                    {
+                        Console.WriteLine("Low 正解です！");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Low ミス！ゲーム終了");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("{0}のスコア:{1}点", me.Name,score);
+            
+        }
+    }
+}
